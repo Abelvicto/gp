0d1b81087b040a1c764287465016645d8e159a4d
Abelvicto <abelchidiebere19@gmail.com>
2025-02-06 02:27:51 +0100

    Initial commit for Netlify deployment

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+function Footer() {
+    return (
+        <footer className='text-center bg-gray-900 w-full py-3 text-white '>
+           Built by @subagents housing
+         
+           
+        </footer>
+    )
+}
+
+export default Footer
